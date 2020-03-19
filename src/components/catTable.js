bd78b4ce7793380d27f869f254896036d5d87cdb
annikjo <annikjo@metropolia.fi>
2020-03-19 10:11:36 +0200

    task a

--- src/components/catTable.js
+++ src/components/catTable.js
@@ -1,23 +1,30 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import PropTypes from "prop-types";
 import CatRow from "./catRow";
 
-const CatTable = ({ media }) => {
+const CatTable = () => {
+  const [picArray, setPicArray] = useState([]);
+ const loadMedia = async () => {
+   const response = await fetch('test.json');
+   const json = await response.json();
+   console.log(json);
+   setPicArray(json);
+ };
+
+ useEffect( () => {
+   loadMedia();
+ }, []);
+
   return (
     <table>
       <tbody>
         {
-            media.map((file, index) => {
-            return <CatRow file={file} key={index} />;
-            })
+            picArray.map((file, index) => <CatRow file={file} key={index} />)
         }
       </tbody>
     </table>
   );
 };
 
-CatTable.propTypes = {
-  media: PropTypes.array
-};
-
 export default CatTable;
+ 
