72a880fbc4c3b0697b70adc7543e544bd16614f1
annikjo <annikjo@metropolia.fi>
2020-03-19 10:54:28 +0200

    task b

--- src/components/catTable.js
+++ src/components/catTable.js
@@ -1,30 +1,34 @@
-import React, {useState, useEffect} from "react";
-import PropTypes from "prop-types";
+import React, { useState, useEffect } from "react";
 import CatRow from "./catRow";
+
+const baseUrl = "http://media.mw.metropolia.fi/wbma/";
 
 const CatTable = () => {
   const [picArray, setPicArray] = useState([]);
- const loadMedia = async () => {
-   const response = await fetch('test.json');
-   const json = await response.json();
-   console.log(json);
-   setPicArray(json);
- };
+  const loadMedia = async () => {
+    const response = await fetch(baseUrl + 'media');
+    const json = await response.json();
+    const items = await Promise.all(json.map(async (item) => {
+        const response = await fetch(baseUrl + 'media/' + item.file_id);
+        return await response.json();        
+      }));
+      console.log(items);
+      setPicArray(items);
+  };
 
- useEffect( () => {
-   loadMedia();
- }, []);
+  useEffect(() => {
+    loadMedia();
+  }, []);
 
   return (
     <table>
       <tbody>
-        {
-            picArray.map((file, index) => <CatRow file={file} key={index} />)
-        }
+        {picArray.map((file, index) => (
+          <CatRow file={file} key={index} />
+        ))}
       </tbody>
     </table>
   );
 };
 
 export default CatTable;
- 
